b26931da88a30cf3c7c0357fcae6f855d230da9e
Software group <admin@sfgroups.com>
2018-07-15 22:18:18 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridNg2 } from 'ag-grid-angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent implements OnInit {
+    @ViewChild('agGrid') agGrid: AgGridNg2;
+    private gridApi;
+    private gridColumnApi;
+
+    title = 'app';
+
+    columnDefs = [
+        {headerName: 'Host Name', field: 'hostname', checkboxSelection: false },
+        {headerName: 'FQDN', field: 'fqdn' },
+        {headerName: 'IP', field: 'ip'},
+        {headerName: 'Domain', field: 'domain'},
+        {headerName: 'Application', field: 'application'},
+        {headerName: 'CPU', field: 'cpu'},
+        {headerName: 'Memory', field: 'memory'},
+        {headerName: 'Owner', field: 'owner'}
+
+    ];
+
+    rowData: any;
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit() {      
+        this.rowData = this.http.get(' http://localhost:4200/assets/servers5.json');      
+    }
+    onBtExport() {
+      var params = {        
+        allColumns: 1,       
+      };      
+      this.gridApi.exportDataAsCsv(params);
+    }
+
+    onGridReady(params) {
+      this.gridApi = params.api;
+      this.gridColumnApi = params.columnApi;
+      params.api.sizeColumnsToFit();
+      
+    }
+  }
+
+  function getBooleanValue(cssSelector) {
+    return document.querySelector(cssSelector).checked === true;
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
+import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AgGridModule.withComponents([])],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
